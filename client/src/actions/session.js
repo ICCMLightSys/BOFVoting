72e30b5bb667c22047d31385b1dd58c7b74074c9
Nathan Bierema <nbierema@gmail.com>
2018-03-05 16:03:26 -0700

    Remove cruft

--- client/src/actions/session.js
+++ client/src/actions/session.js
@@ -1,32 +1,5 @@
-import * as actionTypes from '../constants/actionTypes';
 import configuration from '../configuration';
 import { RECEIVE_SESSIONS } from '../constants/actionTypes';
-
-// export const REQUEST_POSTS = 'REQUEST_POSTS'
-// function requestPosts(subreddit) {
-//     return {
-//         type: REQUEST_POSTS,
-//         subreddit
-//     }
-// }
-// ​
-// export const RECEIVE_POSTS = 'RECEIVE_POSTS'
-// function receivePosts(subreddit, json) {
-//     return {
-//         type: RECEIVE_POSTS,
-//         subreddit,
-//         posts: json.data.children.map(child => child.data),
-//         receivedAt: Date.now()
-//     }
-// }
-// ​
-// export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
-// export function invalidateSubreddit(subreddit) {
-//     return {
-//         type: INVALIDATE_SUBREDDIT,
-//         subreddit
-//     }
-// }
 
 export const fetchSessions = (conferenceId) => {
   return async (dispatch) => {
@@ -46,32 +19,3 @@ const receiveSessions = (sessions) => {
     payload: { sessions },
   },
 };
-​
-// function shouldFetchPosts(state, subreddit) {
-//   const posts = state.postsBySubreddit[subreddit]
-//   if (!posts) {
-//       return true
-//   } else if (posts.isFetching) {
-//       return false
-//   } else {
-//       return posts.didInvalidate
-//   }
-// }
-​
-// export function fetchPostsIfNeeded(subreddit) {
-//     // Note that the function also receives getState()
-//     // which lets you choose what to dispatch next.
-// ​
-//     // This is useful for avoiding a network request if
-//     // a cached value is already available.
-// ​
-//   return (dispatch, getState) => {
-//       if (shouldFetchPosts(getState(), subreddit)) {
-//           // Dispatch a thunk from thunk!
-//           return dispatch(fetchPosts(subreddit))
-//       } else {
-//           // Let the calling code know there's nothing to wait for.
-//           return Promise.resolve()
-//       }
-//   }
-// }
