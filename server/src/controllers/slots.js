981d83670c48147f35d241ed2f58b99a7aaa37cd
Nathan Bierema <nbierema@gmail.com>
2018-03-08 16:43:21 -0700

    Add callback and promise to wait for response

--- server/src/controllers/slots.js
+++ server/src/controllers/slots.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const { requireUserToBeAdmin } = require('../middleware/validation');
 const requireAuthentication = require('../middleware/authentication');
+const generateSchedule = require('../modules/scheduler');
 
 const router = express.Router();
 
 router.post('/conferences/:conferenceId/generateSchedule', requireAuthentication, requireUserToBeAdmin, async (req, res) => {
   // TODO Generate schedule
+  const conference = 0;
+  const sessions = 0;
+
+  await new Promise((resolve) => {
+    generateSchedule(conference, sessions, (pop, gen, stats) => {
+      resolve(pop, gen, stats);
+    });
+  });
 
   res.status(204).send();
 });
